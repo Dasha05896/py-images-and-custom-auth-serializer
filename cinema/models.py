f343931b34a53dcdf32fc58d376158889fe18254
Daria Budovich <Budovich_Dasha@ukr.net>
2026-02-04 13:31:38 +0200

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -1,6 +1,9 @@
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.conf import settings
+import os
+import uuid
+from django.utils.text import slugify
 
 
 class CinemaHall(models.Model):
@@ -34,6 +37,11 @@ class Actor(models.Model):
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
 
+    def movie_image_file_path(self, instance, filename):
+        _, extension = os.path.splitext(filename)
+        filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+        return os.path.join("uploads/movies/", filename)
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
@@ -41,6 +49,7 @@ class Movie(models.Model):
     duration = models.IntegerField()
     genres = models.ManyToManyField(Genre)
     actors = models.ManyToManyField(Actor)
+    image = models.ImageField(null=True, upload_to="movie_image_file_path")
 
     class Meta:
         ordering = ["title"]
--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+from django.contrib.auth import get_user_model
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(get_user_model())
+class CustomUserAdmin(UserAdmin):
+    # Видаляємо username з усіх налаштувань адмінки
+    ordering = ("email",)
+    list_display = ("email", "is_staff", "is_superuser")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_active",
+                                    "is_staff",
+                                    "is_superuser",
+                                    "groups",
+                                    "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password"),
+        }),
+    )
+    search_fields = ("email",)
